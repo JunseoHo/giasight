bb1460f9ad403633cb6db9dfb17acf9ef664e64a
JunseoHo <junseoho1029@gmail.com>
2024-05-02 21:51:20 +0900

    feat : README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Reference 
+
+- [ESLint](https://eslint.org/docs/latest/use/getting-started) : 프론트엔트 코드 품질 관리 툴
---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,13 @@
+import js from "@eslint/js"
+
+export default [
+  {
+    files: ["app/**.tsx"],
+    languageOptions: {
+      parser: "@babel/babel"
+    },
+    rules: {
+
+    }
+  }
+];
